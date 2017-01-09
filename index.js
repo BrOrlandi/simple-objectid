24a67962eb75fa421e742f2a40bbdc091897450f
BrOrlandi <brorlandi@gmail.com>
2017-01-09 10:23:22 -0200

    Update for support ES5

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-module.exports = (m = Math, d = Date, h = 16, s = s => m.floor(s).toString(h)) =>
-s(d.now() / 1000) + ' '.repeat(h).replace(/./g, () => s(m.random() * h));
+'use strict';
+
+module.exports = function () {
+  var m = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : Math;
+  var d = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : Date;
+  var h = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 16;
+  var s = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : function (s) {
+    return m.floor(s).toString(h);
+  };
+  return s(d.now() / 1000) + ' '.repeat(h).replace(/./g, function () {
+    return s(m.random() * h);
+  });
+};
